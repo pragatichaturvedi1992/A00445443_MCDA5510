1e39ed82a09a6789b366129e1851ba2cd1e26012
pragatichaturvedi1992 <77502679+pragatichaturvedi1992@users.noreply.github.com>
2021-03-22 00:21:20 +0530

    newl inclass demo main changes

---
+++ InClassDemo/Models/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace InClassDemo.Models
+{
+    public partial class Customer
+    {
+
+        public Customer()
+        {
+            this.Province = "Nova Scotia";
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Addrses1 { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+
+        [Required]
+        [PostalValidate]
+        public string Postal { get; set; }
+        public string Phone { get; set; }
+
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+
+
+        // This property will hold all available states for selection
+        //     public IEnumerable<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem> provinces { get; set; }
+
+    }
+}
---
+++ InClassDemo/Models/CustomerContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace InClassDemo.Models
+{
+    public partial class CustomerContext : DbContext
+    {
+        public CustomerContext()
+        {
+        }
+
+        public CustomerContext(DbContextOptions<CustomerContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.Addrses1).HasMaxLength(50);
+
+                entity.Property(e => e.City).HasMaxLength(50);
+
+                entity.Property(e => e.Country).HasMaxLength(50);
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+
+                entity.Property(e => e.Phone).HasMaxLength(50);
+
+                entity.Property(e => e.Postal).HasMaxLength(50);
+
+                entity.Property(e => e.Province).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
